d59f07a2321c06b3cda013c2d2cf4f5b36b59816
Natalie Vinyu <135601983+NatalieVinyu@users.noreply.github.com>
2026-04-22 19:38:23 +0200

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='bg-black text-white'>
+      <h1 className='text-4xl'>Notes App</h1>
+    </div>
+  )
+}
+
+export default App
