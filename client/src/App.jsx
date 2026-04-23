8f1b0273c22018c13912baae40fddf694eabd13f
Natalie Vinyu <135601983+NatalieVinyu@users.noreply.github.com>
2026-04-23 09:30:56 +0200

    testing supabase

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import { supabase } from './supabaseClient'
 
 function App() {
+  const [todos, setTodos] = useState([])
+
+  useEffect(() => {
+    getTodos()
+  }, [])
+
+  async function getTodos() {
+    const { data, error } = await supabase
+      .from('todos')
+      .select('*')
+
+    if (error) {
+      console.error('Error fetching todos:', error)
+    } else {
+      setTodos(data)
+      console.log('Supabase working:', data)
+    }
+  }
+
   return (
-    <div className='bg-black text-white'>
-      <h1 className='text-4xl'>Notes App</h1>
+    <div style={{ padding: '20px' }}>
+      <h1>Supabase Test</h1>
+
+      <ul>
+        {todos.map(todo => (
+          <li key={todo.id}>{todo.task}</li>
+        ))}
+      </ul>
     </div>
   )
 }
